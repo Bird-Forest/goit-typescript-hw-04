b167e5c59525e42d9f85b8652371668460e050c6
Bird-Forest <bird.forest70@gmail.com>
2023-11-18 21:34:57 +0200

    add file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import RequestComponent from './homework/2';
+// import { Observer } from './homework/1';
+// import {Props} from './App.types' 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello people!</p>
+      <RequestComponent/>
     </div>
   );
 }
